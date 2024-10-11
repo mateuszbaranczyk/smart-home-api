621dec248b4ac8ec646841bb8865ab75699cd4d8
kundzior <grzybek2413@gmail.com>
2024-10-11 16:48:10 +0200

    to viewset refactor

--- home_api/garlight/views.py
+++ home_api/garlight/views.py
@@ -1,12 +1,9 @@
 from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from rest_framework.generics import (
-    GenericAPIView,
-    ListAPIView,
-    RetrieveUpdateDestroyAPIView,
-)
-
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.request import Request
 
 from garlight.bulbs import discover_bulbs
@@ -14,24 +11,22 @@ from garlight.models import YeelightBulb
 from garlight.serializers import BulbSerializer
 
 
-class BulbView(RetrieveUpdateDestroyAPIView):
+class BulbViewSet(viewsets.ModelViewSet):
     queryset = YeelightBulb.objects.all()
     serializer_class = BulbSerializer
     lookup_field = "name"
 
+    def create(self, request, *args, **kwargs):
+        msg = "Please use 'bulbs/discover/' endpoint for discover and create new device"
+        raise MethodNotAllowed("POST", detail=msg)
 
-class BulbListView(ListAPIView):
-    queryset = YeelightBulb.objects.all()
-    serializer_class = BulbSerializer
-
-
-class DeiscoverView(GenericAPIView):
-    def get(self, request: Request, *args, **kwargs) -> HttpResponseRedirect:
+    @action(detail=False, methods=["get"])
+    def discover(self, request: Request):
         discovered = discover_bulbs()
         existing = YeelightBulb.objects.all().values_list("bulb_id", flat=True)
         bulbs = self._create_db_obj(discovered, existing)
         YeelightBulb.objects.bulk_create(bulbs)
-        return HttpResponseRedirect(reverse("bulb-list"))
+        return HttpResponseRedirect(reverse("yeelightbulb-list"))
 
     def _create_db_obj(
         self, discovered: dict, existing: QuerySet
