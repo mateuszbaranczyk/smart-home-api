942b15dc62271c3114b2a12c65e516936e6129f4
kundzior <grzybek2413@gmail.com>
2024-10-12 11:36:17 +0200

    bulb class refactor

--- home_api/garlight/bulbs.py
+++ home_api/garlight/bulbs.py
@@ -1,13 +1,14 @@
 from yeelight import Bulb, CronType, SceneClass, discover_bulbs
+from garlight.models import YeelightBulb, Color, Temperature
 
 
 class BulbException(Exception):
     pass
 
 
-class HomeBulb:
-    def __init__(self, name: str) -> None:
-        self.model = self.get_from_db(name)
+class SmartBulb:
+    def __init__(self, db_model: YeelightBulb) -> None:
+        self.model = db_model
         self.bulb = Bulb(ip=self.model.ip)
 
     def __repr__(self) -> str:
@@ -17,19 +18,12 @@ class HomeBulb:
     def state(self):
         return f"{self.model.name} - {self.check_state()}"
 
-    def get_from_db(self, name: str):
-        model = db.session.execute(
-            db.select(BulbModel).filter_by(name=name)
-        ).scalar_one()
-        return model
-
     def on_off(self) -> str:
         power = self.check_state()
         try:
             msg = self.change_state(power)
             return f"{msg}"
         except Exception as err:
-            gunicorn_logger.error(f"Bulb - {self.bulb_name} - {err}")
             raise BulbException(err)
 
     def change_state(self, power_status: str) -> str:
@@ -55,50 +49,25 @@ class HomeBulb:
             return f"Timer to {minutes} min."
         return "Failed"
 
-    def set_color(self, red: int, green: int, blue: int, brightness: int) -> str:
-        """Colors in range 0-255, brightness 0-100"""
-        self._validate_colors(red, green, blue, brightness)
-        status = self.bulb.set_scene(SceneClass.COLOR, red, green, blue, brightness)
+    def set_color(self, color: Color) -> str:
+        status = self.bulb.set_scene(
+            SceneClass.COLOR, color.r, color.g, color.b, color.brightness
+        )
         return self._status_return(status)
 
-    def set_temperature(self, temperature: int, brightness: int) -> str:
+    def set_temperature(self, temperature: Temperature) -> str:
         """Temperature in range 1700-6500, brightness 0-100"""
-        self._validate_temperature(temperature, brightness)
-        status = self.bulb.set_scene(SceneClass.CT, temperature, brightness)
+        status = self.bulb.set_scene(
+            SceneClass.CT, temperature.kelvins, temperature.brightness
+        )
         return self._status_return(status)
 
     def _status_return(sefl, status: str) -> str:
         if status == "ok":
-            return str.capitalize(status)
+            return "Ok"
         return "Failed"
-
-    def _validate_temperature(sefl, temperature: int, brightness: int) -> None:
-        if temperature not in range(1700, 6501):
-            raise ValueError("Temperature out of range!")
-
-        if brightness not in range(0, 101):
-            raise ValueError("Brightness out of range!")
-
-    def _validate_colors(
-        self, red: int, green: int, blue: int, brightness: int
-    ) -> None:
-        if red not in range(0, 256):
-            raise ValueError("Red out of range!")
-        if green not in range(0, 256):
-            raise ValueError("Green out of range!")
-        if blue not in range(0, 256):
-            raise ValueError("Blue out of range!")
-        if brightness not in range(0, 101):
-            raise ValueError("Brightness out of range!")
 
 
 def discover_and_assign() -> None:
     devices = discover_bulbs()
     return devices
-
-
-def bulb_exists(bulb_id: str) -> bool:
-    exists = db.session.query(literal(True)).filter(BulbModel.id == bulb_id).first()
-    if exists:
-        return exists[0]
-    return False
