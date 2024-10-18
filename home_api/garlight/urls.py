0378cb08cc751ac9a99cf4e6f84f0efba8121612
kundzior <grzybek2413@gmail.com>
2024-10-18 17:37:25 +0200

    mv urls to app

---
+++ home_api/garlight/urls.py
@@ -0,0 +1,20 @@
+from garlight import views
+from rest_framework.routers import DefaultRouter
+
+app_router = DefaultRouter()  # management
+device_router = DefaultRouter()  # api for sending requests to devices
+
+app_router.register(r"bulbs", views.BulbViewSet, basename="bulb")
+app_router.register(r"on-off", views.BulbPowerViewSet, basename="bulb-power")
+app_router.register(r"color", views.BulbColorViewSet, basename="bulb-color")
+app_router.register(r"temperature", views.BulbTemperatureViewSet, basename="bulb-temp")
+app_router.register(r"timer", views.BulbTimerViewSet, basename="bulb-timer")
+app_router.register(r"endpoint", views.EndpointViewSet, basename="bulb-endpoint")
+
+device_router.register(r"colors", views.ColorViewSet, basename="colors")
+device_router.register(r"temperatures", views.TemperatureViewSet, basename="temperatures")
+device_router.register(r"timers", views.TimerViewSet, basename="timers")
+device_router.register(r"endpoints", views.GarminEndpointsViewSet, basename="endpoints")
+
+
+garlight_urls = app_router.urls + device_router.urls
--- home_api/home_api/urls.py
+++ home_api/home_api/urls.py
@@ -1,22 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from garlight import views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register(r"bulbs", views.BulbViewSet, basename="bulb")
-router.register(r"on-off", views.BulbPowerViewSet, basename="bulb-power")
-router.register(r"color", views.BulbColorViewSet, basename="bulb-color")
-router.register(r"temperature", views.BulbTemperatureViewSet, basename="bulb-temp")
-router.register(r"timer", views.BulbTimerViewSet, basename="bulb-timer")
-router.register(r"endpoint", views.EndpointViewSet, basename="bulb-endpoint")
-
-router.register(r"colors", views.ColorViewSet, basename="colors")
-router.register(r"temperatures", views.TemperatureViewSet, basename="temperatures")
-router.register(r"timers", views.TimerViewSet, basename="timers")
-router.register(r"endpoints", views.GarminEndpointsViewSet, basename="garmin-endpoints")
+from garlight.urls import garlight_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-] + router.urls
+] + garlight_urls
