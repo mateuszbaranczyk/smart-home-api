ea1c7d03b67854d3bfbd98b3df8f415e5f7df9f4
kundzior <grzybek2413@gmail.com>
2024-10-18 16:48:28 +0200

    register models in admin

--- home_api/garlight/admin.py
+++ home_api/garlight/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from garlight.models import Color, Temperature, YeelightBulb
+from garlight.models import Color, Temperature, YeelightBulb, Timer, Endpoint
 
 admin.site.register(YeelightBulb)
 admin.site.register(Color)
 admin.site.register(Temperature)
+admin.site.register(Timer)
+admin.site.register(Endpoint)
