4f4426d118f64944053cddc50f4dfb5bcb239652
kundziorro <grzybek2413@gmail.com>
2024-10-31 18:31:58 +0100

    Register Brightness model in Django admin

--- home_api/garlight/admin.py
+++ home_api/garlight/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from garlight.models import Color, Endpoint, Temperature, Timer, YeelightBulb
+from garlight.models import Color, Endpoint, Temperature, Timer, YeelightBulb, Brightness
 
 admin.site.register(YeelightBulb)
 admin.site.register(Color)
 admin.site.register(Temperature)
 admin.site.register(Timer)
+admin.site.register(Brightness)
 admin.site.register(Endpoint)
