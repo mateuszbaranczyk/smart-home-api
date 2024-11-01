9dce411518ad86392cfebed28fa22d8109cbc04f
kundziorro <grzybek2413@gmail.com>
2024-11-01 14:41:28 +0100

    fixed urls assigment

--- home_api/garlight/urls.py
+++ home_api/garlight/urls.py
@@ -6,16 +6,16 @@ device_router = DefaultRouter()  # api for sending requests to devices
 
 # fmt: off
 app_router.register(r"bulbs", views.BulbViewSet, basename="bulb")
-app_router.register(r"on-off", views.BulbPowerViewSet, basename="bulb-power")
-app_router.register(r"color", views.BulbColorViewSet, basename="bulb-color")
-app_router.register(r"temperature", views.BulbTemperatureViewSet, basename="bulb-temp")
-app_router.register(r"timer", views.BulbTimerViewSet, basename="bulb-timer")
+app_router.register(r"colors", views.ColorViewSet, basename="colors")
+app_router.register(r"temperatures", views.TemperatureViewSet, basename="temperatures")
+app_router.register(r"timers", views.TimerViewSet, basename="timers")
 app_router.register(r"brightness", views.BrightnessViewSet, basename="bulb-brightness")
 app_router.register(r"endpoint", views.EndpointViewSet, basename="bulb-endpoint")
 
-device_router.register(r"colors", views.ColorViewSet, basename="colors")
-device_router.register(r"temperatures", views.TemperatureViewSet, basename="temperatures")
-device_router.register(r"timers", views.TimerViewSet, basename="timers")
+device_router.register(r"on-off", views.BulbPowerViewSet, basename="bulb-power")
+device_router.register(r"color", views.BulbColorViewSet, basename="bulb-color")
+device_router.register(r"temperature", views.BulbTemperatureViewSet, basename="bulb-temp")
+device_router.register(r"timer", views.BulbTimerViewSet, basename="bulb-timer")
 device_router.register(r"brightnesses", views.BrightnessViewSet, basename="brightnesses")
 device_router.register(r"endpoints", views.GarminEndpointsViewSet, basename="endpoints")
 # fmt: on
