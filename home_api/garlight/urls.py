cfd89e4771cc9c495df09746e44eeda2c634f7d2
kundziorro <grzybek2413@gmail.com>
2024-11-01 14:59:31 +0100

    fix: update brightness route names for consistency

--- home_api/garlight/urls.py
+++ home_api/garlight/urls.py
@@ -9,14 +9,14 @@ app_router.register(r"bulbs", views.BulbViewSet, basename="bulb")
 app_router.register(r"colors", views.ColorViewSet, basename="colors")
 app_router.register(r"temperatures", views.TemperatureViewSet, basename="temperatures")
 app_router.register(r"timers", views.TimerViewSet, basename="timers")
-app_router.register(r"brightness", views.BrightnessViewSet, basename="bulb-brightness")
+app_router.register(r"brightnesses", views.BrightnessViewSet, basename="bulb-brightnesses")
 app_router.register(r"endpoint", views.EndpointViewSet, basename="bulb-endpoint")
 
 device_router.register(r"on-off", views.BulbPowerViewSet, basename="bulb-power")
 device_router.register(r"color", views.BulbColorViewSet, basename="bulb-color")
 device_router.register(r"temperature", views.BulbTemperatureViewSet, basename="bulb-temp")
 device_router.register(r"timer", views.BulbTimerViewSet, basename="bulb-timer")
-device_router.register(r"brightnesses", views.BrightnessViewSet, basename="brightnesses")
+device_router.register(r"brightness", views.BrightnessViewSet, basename="brightness")
 device_router.register(r"endpoints", views.GarminEndpointsViewSet, basename="endpoints")
 # fmt: on
 
