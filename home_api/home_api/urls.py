a9f78be6d5f2430dc205c4eac678065c582b12b1
kundziorro <mateusz.baranczyk@gmail.com>
2024-11-02 20:52:33 +0100

    static setup

--- home_api/home_api/urls.py
+++ home_api/home_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from garlight.urls import garlight_urls
+from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-] + garlight_urls
+] + garlight_urls + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
