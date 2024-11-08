41b50c598bbb53f9e7e19479a26aceb8f7c4ee93
kundziorro <mateusz.baranczyk@gmail.com>
2024-11-08 18:53:24 +0100

    add authentication view with dynamic permission classes based on developer mode

--- home_api/authentication/views.py
+++ home_api/authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.conf import settings
 
-# Create your views here.
+from rest_framework.views import APIView
+
+
+class Auth(APIView):
+    is_developer = settings.DEVELOPER
+    permission_classes = [
+        IsAuthenticated if is_developer == "False" else AllowAny
+    ]
