faad7db8c8a89655813cc14281c765269f35aa21
kundziorro <mateusz.baranczyk@gmail.com>
2024-11-08 20:36:42 +0100

    urls

--- home_api/home_api/urls.py
+++ home_api/home_api/urls.py
@@ -1,13 +1,15 @@
+from authentication.urls import urlpatterns as authentication_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from garlight.urls import garlight_urls
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
     ]
     + garlight_urls
+    + authentication_urls
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
