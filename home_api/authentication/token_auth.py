f68e6f96a9feff1fa4ad65f2b30365769dde9c6c
kundziorro <mateusz.baranczyk@gmail.com>
2024-11-26 17:31:11 +0100

    token auth class override

---
+++ home_api/authentication/token_auth.py
@@ -0,0 +1,37 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
+from rest_framework.authentication import TokenAuthentication
+
+
+class ApiKeyAuth(TokenAuthentication):
+    def authenticate(self, request):
+        auth = self.get_x_api_key_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            return None
+
+        if len(auth) == 1:
+            msg = _("Invalid token header. No credentials provided.")
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _(
+                "Invalid token header. Token string should not contain spaces."
+            )
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _(
+                "Invalid token header. Token string should not contain invalid characters."
+            )
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
+
+    def get_x_api_key_header(self, request):
+        auth = request.META.get("X-API-KEY", b"")
+        if isinstance(auth, str):
+            # Work around django test client oddness
+            auth = auth.encode(HTTP_HEADER_ENCODING)
+        return auth
