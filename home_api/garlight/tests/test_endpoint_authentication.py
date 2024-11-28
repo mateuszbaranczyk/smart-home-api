994cb1e3a933a0c32db2c8d42e8ece96d56362eb
kundziorro <mateusz.baranczyk@gmail.com>
2024-11-28 22:11:40 +0100

    add tests for token authentication errors

--- home_api/garlight/tests/test_endpoint_authentication.py
+++ home_api/garlight/tests/test_endpoint_authentication.py
@@ -28,11 +28,34 @@ class AuthenticationTest(TestCase):
         assert response.status_code == 401
         assert response.content == expected
 
+    def test_raise_error_on_empty_token(self):
+        expected = (
+            b'{"detail":"Invalid token header. No credentials provided."}'
+        )
+        headers = {"AUTHORIZATION": "Token"}
+        response = self.client.get(self.url, headers=headers)
+        assert response.status_code == 401
+        assert response.content == expected
+
+    def test_raise_error_on_invalid_characters(self):
+        expected = b'{"detail":"Invalid token header. Token string should not contain invalid characters."}'
+        headers = {"AUTHORIZATION": b"Token \xff\xff\xff\xff"}
+        response = self.client.get(self.url, headers=headers)
+        assert response.status_code == 401
+        assert response.content == expected
+
     def test_raise_error_on_invalid_token(self):
         headers = {"AUTHORIZATION": "Token invalid_token"}
         response = self.client.get(self.url, headers=headers)
         assert response.status_code == 401
         assert response.content == b'{"detail":"Invalid token."}'
+
+    def test_raise_error_on_token_with_spaces(self):
+        expected = b'{"detail":"Invalid token header. Token string should not contain spaces."}'
+        headers = {"AUTHORIZATION": "Token invalid token"}
+        response = self.client.get(self.url, headers=headers)
+        assert response.status_code == 401
+        assert response.content == expected
 
     def test_pass_on_valid_token(self):
         patch("garlight.bulbs.SmartBulb.on_off", return_value="test").start()
