807b9b76501122f0ad9023e70bda050a7324ad75
kundziorro <mateusz.baranczyk@gmail.com>
2025-01-02 20:41:10 +0100

    location model

--- aura/models.py
+++ aura/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    api_key = models.CharField(max_length=50)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    name = models.CharField(max_length=50)
+    air_quality = models.BooleanField()
