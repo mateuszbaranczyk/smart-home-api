7acb208fcc9b069735188cf3a8a1df8d17a259db
kundziorro <mateusz.baranczyk@gmail.com>
2025-01-02 20:41:02 +0100

    test for adapter

--- aura/tests/tests.py
+++ aura/tests/tests.py
@@ -1,22 +1,24 @@
-from unittest.mock import MagicMock
-
 from django.test import TestCase
 
+from aura.adapters import AdapterResponse, WeatherAdapter, WeatherForecast
+from aura.models import Location
 from aura.tests.response import eg_forecast_response
 
 
 class WeatherAdapterTest(TestCase):
     def setUp(self):
-        self.model = MagicMock()
-        self.model.key = 'test_key'
-        self.model.lat = 20.0
-        self.model.lon = 20.0
-        self.model.name = 'city_name'
-        self.model.air = True 
-        self.adapter = MagicMock()
-        self.adapter.get_weather.return_value=eg_forecast_response
+        self.model = Location(
+            name="test", lat=20, lon=20, api_key="test", air_quality=False
+        )
+        self.adapter = WeatherAdapter(self.model)
 
+
+    #@mock request
     def test_get_weather(self):
+        expected_response_data = WeatherForecast(**eg_forecast_response)
+        self.adapter.get_weather.return_value = AdapterResponse(
+            status_code=200, data=expected_response_data
+        )
         adapter_response = self.adapter.get_weather(self.model)
         assert adapter_response.status_code == 200
-        assert adapter_response.json() == eg_forecast_response
+        assert adapter_response.data.location.name == "Piastow"
