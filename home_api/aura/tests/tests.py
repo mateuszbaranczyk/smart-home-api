ff1dca10495e9a496589aa724af5dcc57aac84f4
kundziorro <mateusz.baranczyk@gmail.com>
2025-01-03 21:08:33 +0100

    refactor: update tests to use renamed Location_ model in WeatherAdapterTest

--- home_api/aura/tests/tests.py
+++ home_api/aura/tests/tests.py
@@ -1,19 +1,17 @@
-from django.test import TestCase
-
 from aura.adapters import AdapterResponse, WeatherAdapter, WeatherForecast
-from aura.models import Location
+from aura.models import Location_
 from aura.tests.response import eg_forecast_response
+from django.test import TestCase
 
 
 class WeatherAdapterTest(TestCase):
     def setUp(self):
-        self.model = Location(
+        self.model = Location_(
             name="test", lat=20, lon=20, api_key="test", air_quality=False
         )
         self.adapter = WeatherAdapter(self.model)
 
-
-    #@mock request
+    # @mock request
     def test_get_weather(self):
         expected_response_data = WeatherForecast(**eg_forecast_response)
         self.adapter.get_weather.return_value = AdapterResponse(
