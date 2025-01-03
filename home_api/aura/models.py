62ba125aea9ed1ad08c67f30ba191c218c77f18b
kundziorro <mateusz.baranczyk@gmail.com>
2025-01-03 21:08:04 +0100

    rename Location model to Location_ and reorder fields

--- home_api/aura/models.py
+++ home_api/aura/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class Location(models.Model):
+class Location_(models.Model):
+    name = models.CharField(max_length=50)
     api_key = models.CharField(max_length=50)
     lat = models.FloatField()
     lon = models.FloatField()
-    name = models.CharField(max_length=50)
     air_quality = models.BooleanField()
