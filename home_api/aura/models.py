68551ee676f5ceeaed3346fb0a77d1d04ecc54b8
kundziorro <grzybek2413@gmail.com>
2025-01-16 21:01:17 +0100

    migrations

--- home_api/aura/models.py
+++ home_api/aura/models.py
@@ -3,7 +3,5 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=50)
-    api_key = models.CharField(max_length=50)
     lat = models.FloatField()
     lon = models.FloatField()
-    air_quality = models.BooleanField()
